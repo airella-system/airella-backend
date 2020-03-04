0af5c61b82b2a83c0605a880e05564012a9bff9b
jakubdybczak <jakub.dybczak@gmail.com>
2020-03-04 01:23:47 +0100

    Init commit

---
+++ src/main/java/pl/edu/agh/airsystem/AirsystemApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.airsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirsystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/agh/airsystem/AirsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.airsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirsystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
