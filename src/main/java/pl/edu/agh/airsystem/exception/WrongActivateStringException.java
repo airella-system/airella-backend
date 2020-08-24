e4d7225baa6621beda70257add49204350bd377f
jakubdybczak <jakub.dybczak@gmail.com>
2020-08-25 00:19:04 +0200

    Fix email title

--- src/main/java/pl/edu/agh/airsystem/exception/WrongActivateStringException.java
+++ src/main/java/pl/edu/agh/airsystem/exception/WrongActivateStringException.java
@@ -5,7 +5,7 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 public class WrongActivateStringException extends BaseErrorException {
     public WrongActivateStringException() {
         super(BAD_REQUEST,
-                "WRONG_ACTIVATE_STRING",
-                "This activation text is wrong.");
+                "WRONG_ACTIVATION_CODE",
+                "This activation code is wrong.");
     }
 }
